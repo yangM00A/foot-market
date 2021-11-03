0bac42973da409c7791f7903ca2c85ae6cc281c1
yangmeng <yangmeng_mail@163.com>
2021-11-03 12:38:21 +0800

    添加侧边菜单简单样式

--- components/verticalnav/verticalnav.js
+++ components/verticalnav/verticalnav.js
@@ -10,6 +10,8 @@ Component({
    * 组件的初始数据
    */
   data: {
+    actvieClass: "menuActive menu",
+    notActiveClass: "meun"
 
   },
 
@@ -17,6 +19,9 @@ Component({
    * 组件的方法列表
    */
   methods: {
-
+    selectMenu(event) {
+      console.log(event);
+      console.log(event.target.dataset);
+    }
   }
 })
